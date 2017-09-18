27690cbe7ed49b8262cd1a1b56a1f9165355696e
n0ne <new.new.none@gmail.com>
2017-09-18 22:32:02 +0200

    Add template filter to shorten text in the middle.

--- URLInspectorApp/templatetags/ui_utils.py
+++ URLInspectorApp/templatetags/ui_utils.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from math import floor, ceil
 
 from django import template
 from django.template.defaultfilters import stringfilter
@@ -6,9 +7,13 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 
-@register.filter("http_code_phrase")
+@register.filter()
 @stringfilter
 def http_code_phrase(code):
+    """
+    :param code: an HTTP status code (e.g. 200, 403, 404, ...).
+    :return: a short name for an HTTP status code, or an empty string if no such code exists.
+    """
     if code.isdigit():
         try:
             return HTTPStatus(int(code)).phrase
@@ -21,6 +26,10 @@ def http_code_phrase(code):
 @register.filter("http_code_desc")
 @stringfilter
 def http_code_description(code):
+    """
+    :param code: an HTTP status code (e.g. 200, 403, 404, ...).
+    :return: a short description for an HTTP status code, or an empty string if no such code exists.
+    """
     if code.isdigit():
         try:
             return HTTPStatus(int(code)).description
@@ -28,3 +37,30 @@ def http_code_description(code):
             pass
 
     return ""
+
+
+@register.filter()
+@stringfilter
+def truncate_chars_middle(text, limit, sep="..."):
+    """
+    Truncates a given string **text** in the middle, so that **text** has length **limit** if it exceeds the number of
+    characters, or else **len(text)** if it doesn't.
+    Since this is a template filter, no exceptions are raised when they would normally do.
+
+    :param text: the text to truncate.
+    :param limit: the maximum length of **text**.
+    :param sep: the separator to display in place of the (**len(text) - limit**) truncated characters.
+    :return: a truncated version of **text**.
+    """
+    if not text or limit < 0:
+        return ""
+
+    length = len(text)
+
+    if length < limit:
+        return text
+    else:
+        first_half = ceil(limit / 2)
+        second_half = length - floor(limit / 2)
+
+        return text[:first_half] + sep + text[second_half:]
--- URLInspectorApp/tests.py
+++ URLInspectorApp/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from URLInspectorApp.templatetags.ui_utils import truncate_chars_middle
 
-# Create your tests here.
+
+class UIUtilsTest(TestCase):
+    def test_truncate_middle(self):
+        expected = {
+            ("sunnyday", 4): "suay",
+            ("wonderful", 3): "wol",
+            ("magician", 5): "magan",
+            ("dinosaur", 7): "dinoaur",
+            ("greek god", 5): "greod",
+            ("all of it", 20): "all of it",
+            ("none of it", 0): "",
+            # Edge cases below
+            ("", 0): "",
+            ("", 44): "",
+            ("willnotbecomputed", -1): "",
+            (False, 5): "False",    # Since the @stringfilter decorator was used, False is turned into "False"
+            (0, 4): "0",
+        }
+
+        for input, output in expected.items():
+            shortened = truncate_chars_middle(*input, sep="")
+
+            self.assertEqual(shortened, output)
