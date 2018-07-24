dcc30ac976604c976fef9bc3c6dc93af117ec0c7
Acsor <nildexo@yandex.com>
2018-07-24 17:02:06 +0200

    Highlight navigation bar items when visiting respective page.

--- URLInspectorApp/urls.py
+++ URLInspectorApp/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "url_inspector"
 
 urlpatterns = [
-    url(r"^$", views.IndexView.as_view(), name="index"),
+    url(r"^index$", views.IndexView.as_view(), name="index"),
     url(r"^inspection/(?P<pk>\d+)", views.InspectionView.as_view(), name="inspection"),
     url(r"^inspections$", views.SavedInspectionsView.as_view(), name="inspections_saved"),
     url(r"^pre_new_inspection", views.PreNewInspectionView.as_view(), name="inspection_pre_new"),
